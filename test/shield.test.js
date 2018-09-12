98c717bc3e12eca275d07c880e3a05ae7b311fff
agius <andrew@atevans.com>
2018-09-12 11:00:46 -0700

    Add high-entropy-string rules

--- test/shield.test.js
+++ test/shield.test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
+
 const tape = require('tape');
 
 const Shield = require('../lib/shield');
@@ -34,11 +37,30 @@ function noProblemo(input, config) {
   });
 }
 
-shieldTape('my aws key: AKIA0123456789123456');
-shieldTape('my aws secret: 0123456789012345678901234567890123456789');
-shieldTape('my mapbox key sk.e01234567890123456789.0123456789012345678912');
+function loadAsset(filename) {
+  return fs.readFileSync(path.resolve(__dirname, 'assets', filename)).toString().trim();
+}
 
-noProblemo('an hyperlink: https://www.AKIA0123456789123456.com');
-noProblemo('an HTML: HTMLStuffThingElement');
-noProblemo('an camelCase: camelCaseStringAkia0123456');
-noProblemo('aws falsepositives: cloudformation:CancelUpdateStack');
+shieldTape('[regex-rule] aws key: AKIA0123456789123456');
+shieldTape('[regex-rule] aws secret: 0123456789012345678901234567890123456789');
+shieldTape('[regex-rule] mapbox key sk.e01234567890123456789.0123456789012345678912');
+
+noProblemo('[regex-remove] an hyperlink: https://www.AKIA0123456789123456.com');
+noProblemo('[regex-remove] HTMLStuffThingElement');
+noProblemo('[regex-remove] camelCaseStringAkia0123456');
+noProblemo('[strings-remove] aws falsepositives: cloudformation:CancelUpdateStack');
+
+shieldTape('[entropy-rule] high entropy string: ' + loadAsset('high-entropy.txt'), 'verydeep');
+
+tape('[length-exclude] excludes long strings without spaces', (t) => {
+  const shield = new Shield();
+
+  shield.configure('verydeep').then(() => {
+    const findings = shield.processString(loadAsset('long-high-entropy.txt'));
+    t.equal(findings.length, 0, 'Did not find a problem');
+    t.end();
+  }).catch((err) => {
+    t.ifErr(err);
+    t.end();
+  });
+});
